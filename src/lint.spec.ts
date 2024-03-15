bedf43afbdbc4422b4ef73f709dd7305f96b68bf
Michael Huang <mikhuang@gmail.com>
2024-03-15 10:01:20 -0700

    walk

--- src/lint.spec.ts
+++ src/lint.spec.ts
@@ -3,5 +3,9 @@ import t from "tap";
 import { lintFolder } from "./lint";
 
 t.test("lint", async (t) => {
-  t.equal(lintFolder("../fixtures/app01"), true, "lint() should return true");
+  t.equal(
+    await lintFolder("./fixtures/app01"),
+    true,
+    "lint() should return true"
+  );
 });
--- src/lint.ts
+++ src/lint.ts
@@ -1,8 +1,46 @@
+import fs from "node:fs";
+import path from "node:path";
+import isString from "lodash/isString";
+import get from "lodash/get";
+import acorn from "acorn";
+import jsx from "acorn-jsx";
+
+import walk from "acorn-walk";
+import { extend } from "acorn-jsx-walk";
+
+extend(walk.base);
+
+async function lintFile(filepath: string) {
+  console.log("linting", filepath);
+  const fileContent = fs.readFileSync(filepath, "utf-8");
+  const parsed = acorn.Parser.extend(jsx()).parse(fileContent, {
+    ecmaVersion: "latest",
+  });
+  walk.simple(parsed, {
+    // @ts-expect-error
+    JSXElement: (node) => {
+      console.log("@", node.openingElement.name.name);
+    },
+  });
+}
+
 /**
  * Perform linting on a folder
- * @param path to root of Toolscript folder
+ * @param dir to root of Toolscript folder
  * @returns lint issues
  */
-export function lintFolder(path: string) {
+export async function lintFolder(dir: string) {
+  // read all files in folder
+  const root = path.resolve(dir);
+
+  const filePaths = fs
+    .readdirSync(root, { recursive: true })
+    .filter((file) => isString(file)) as string[];
+
+  // loop RSX files
+  for (const file of filePaths.filter((file) => file.endsWith(".rsx"))) {
+    await lintFile(path.join(root, file));
+  }
+
   return true;
 }
